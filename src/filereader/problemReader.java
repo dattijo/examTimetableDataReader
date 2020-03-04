997c8b4018d37616386bf55e9f456828b682358a
PhDLab <PhDLab@DESKTOP-0QKGPRD>
2020-03-04 16:43:30 +0100

    Used Inner Class

--- src/filereader/problemReader.java
+++ src/filereader/problemReader.java
@@ -21,26 +21,28 @@ import java.util.Map;
  * @author PhDLab
  */
 
-class Exam
-{
-    int examId,examDuration,studentsCount=0;
-    ArrayList<Integer> enrollmentList = new ArrayList<>();
-    
-    Exam(int id, int duration)
-    {
-        examId=id;
-        examDuration=duration;
-    }
-    
-    void addStudent(Integer student)
-    {
-        enrollmentList.add(student);
-        studentsCount++;
-    }
-}
+
 
 public class problemReader
 {      
+    class Exam
+    {
+        int examId,examDuration,studentsCount=0;
+        ArrayList<Integer> enrollmentList = new ArrayList<>();
+
+        Exam(int id, int duration)
+        {
+            examId=id;
+            examDuration=duration;
+        }
+
+        void addStudent(Integer student)
+        {
+            enrollmentList.add(student);
+            studentsCount++;
+        }
+    }
+    
     int numberOfExams,numberOfPeriods,numberOfRooms;
     
     Map <Integer,List> studentMap = new HashMap<>();
@@ -116,17 +118,15 @@ public class problemReader
                 switch(t)
                 {                    
                     case StreamTokenizer.TT_EOL:    
-                        //line = tok.lineno()-1;
-                        //System.out.println("Finished Reading Exam "+ (line-1) + "\n" + examVector.get(line-2).studentsCount + " student(s) added : " + examVector.get(line-2).enrollmentList);
                         tok.nextToken(); 
-                        System.out.println("Now reading: "+(int)tok.nval);
+                        //System.out.println("Now reading: "+(int)tok.nval);
                         addExam(tok);                        
                         break;
                     case StreamTokenizer.TT_NUMBER: 
                         
                         Integer currentStudent = (int)tok.nval;                 
                         examVector.get(tok.lineno()-2).addStudent(currentStudent);
-                        System.out.println("Student "+(int)tok.nval+" added successfully.");
+                        //System.out.println("Student "+(int)tok.nval+" added successfully.");
                         if(!studentMap.containsKey(currentStudent))
                         {                            
                             List <Integer> examList = new ArrayList();
@@ -142,17 +142,19 @@ public class problemReader
         int studentCount=0;
         for(Map.Entry<Integer,List> entry : studentMap.entrySet())            
         {
-            System.out.println("Student "+ (++studentCount) + "{ " + entry.getKey() + "}: Exams = " + entry.getValue());
+            //System.out.println("Student "+ (++studentCount) + "{ " + entry.getKey() + "}: Exams = " + entry.getValue());
         }
 
         //Initialize Conflict Matrix
-        ArrayList <ArrayList<Integer>> conflictMatrix = new ArrayList<>(numberOfExams);
+        int matrix[][]= new int[numberOfExams][numberOfExams];
+        //ArrayList <ArrayList<Integer>> conflictMatrix = new ArrayList<>(numberOfExams);
         for(int i=0;i<=numberOfExams-1;i++)
         {            
-            conflictMatrix.add(new ArrayList(numberOfExams));
+            //conflictMatrix.add(new ArrayList(numberOfExams));
             for(int j=0;j<=numberOfExams-1;j++)
             {
-                conflictMatrix.get(i).add(0);
+                matrix[i][j]=0;
+                //conflictMatrix.get(i).add(0);
             }
         }
         //Generate Conflict Matrix
@@ -162,7 +164,7 @@ public class problemReader
         
         for(int currExam=0; currExam<=examVector.size()-2;currExam++)
         {                        
-            System.out.println("Current Exam: " + examVector.get(currExam).examId+ " with "+ examVector.get(currExam).enrollmentList.size()+" students");
+            //System.out.println("Current Exam: " + examVector.get(currExam).examId+ " with "+ examVector.get(currExam).enrollmentList.size()+" students");
             int student;
             cleared.clear();
             for(int currStudent=0; currStudent<=examVector.get(currExam).enrollmentList.size()-1;currStudent++)
@@ -170,40 +172,52 @@ public class problemReader
                 student = examVector.get(currExam).enrollmentList.get(currStudent);
                 if(cleared.contains(student))continue;
                 cleared.add(student);
-                System.out.println("Current Student: " + student);
-                //int conflictCount=0;
+                //System.out.println("Current Student: " + student);
                 for(int nextExam=currExam+1;nextExam<=examVector.size()-1;nextExam++)
                 {                   
-                    System.out.println("Next Exam: " + examVector.get(nextExam).examId);
+                    //System.out.println("Next Exam: " + examVector.get(nextExam).examId);
                     if(examVector.get(nextExam).enrollmentList.contains(student))
                     {
-                        //conflictCount++;
-                        System.out.println("Student "+student +" found in both exams "+ currExam +" and "+ nextExam);
-                        System.out.println();
-                        int tmpEnrollment =  conflictMatrix.get(currExam).get(nextExam);
-                        System.out.println("Previous conflict :"+tmpEnrollment);
-                        tmpEnrollment++;  
-                        conflictMatrix.get(currExam).set(nextExam, tmpEnrollment);
-                        conflictMatrix.get(nextExam).set(currExam, tmpEnrollment);
-                        //conflictMatrix.get(currExam).remove(nextExam);
-                        //conflictMatrix.get(currExam).add(nextExam, tmpEnrollment);
-                        //conflictMatrix.get(currExam).remove(currExam);
-                        //conflictMatrix.get(nextExam).add(currExam, tmpEnrollment);
+                        //System.out.println("Student "+student +" found in both exams "+ currExam +" and "+ nextExam);                        
+                        //int tmpEnrollment =  conflictMatrix.get(currExam).get(nextExam);
+                        int conflictCount = matrix[currExam][nextExam];
+                        
+                        //tmpEnrollment++;  
+                        conflictCount++;
+                        
+                        //conflictMatrix.get(currExam).set(nextExam, tmpEnrollment);                        
+                        //conflictMatrix.get(nextExam).set(currExam, tmpEnrollment);
+                        
+                        matrix[currExam][nextExam]=conflictCount;
+                        matrix[nextExam][currExam]=conflictCount;
                     }
                 }
             }
         }
 
-        //Display ConflictMatrix
-        System.out.println("DISPLAYING CONFLICT MARIX:\n");
-        for(int i=0;i<numberOfExams;i++)
-        {
-            for(int j=0;j<numberOfExams;j++)
-            {
-                System.out.print(conflictMatrix.get(i).get(j)+", ");
-            }
-            System.out.println();
-        }  
+        //ArrayList ConflictMatrix
+//        System.out.println("DISPLAYING ArrayList CONFLICT MARIX:\n");
+//        for(int i=0;i<numberOfExams;i++)
+//        {
+//            for(int j=0;j<numberOfExams;j++)
+//            {
+//                System.out.print(conflictMatrix.get(i).get(j)+", ");
+//                //System.out.print(matrix[i][j]+", ");
+//            }
+//            System.out.println();
+//        }  
+        
+        //int Matrix ConflictMatrix
+//        System.out.println("DISPLAYING int[][] Matrix CONFLICT MARIX:\n");
+//        for(int i=0;i<numberOfExams;i++)
+//        {
+//            for(int j=0;j<numberOfExams;j++)
+//            {
+//                //System.out.print(conflictMatrix.get(i).get(j)+", ");
+//                System.out.print(matrix[i][j]+", ");
+//            }
+//            System.out.println();
+//        } 
     }
     
     void addExam(StreamTokenizer tok)            
@@ -212,10 +226,10 @@ public class problemReader
         int line = tok.lineno()-1;
         if(line<=numberOfExams)
         {
-            System.out.println("Token is @ line: "+tok.lineno());
+            //System.out.println("Token is @ line: "+tok.lineno());
             examVector.add(new Exam(line-1,(int)tok.nval));        
-            System.out.println("Exam "+ (line-1) + " added. Duration = " + examVector.get(line-1).examDuration);  
-            examVector.forEach((e)->System.out.println(e.examId));
+            //System.out.println("Exam "+ (line-1) + " added. Duration = " + examVector.get(line-1).examDuration);  
+            //examVector.forEach((e)->System.out.println(e.examId));
         }                
     }
     
@@ -367,7 +381,7 @@ public class problemReader
     {
         try
         {
-            problemReader objproblemReader = new problemReader("C:/Users/PhDLab/Documents/NetBeansProjects/examTimetableDataReader/exam_comp_set00.exam");
+            problemReader objproblemReader = new problemReader("C:/Users/PhDLab/Documents/NetBeansProjects/examTimetableDataReader/exam_comp_set7.exam");
         }
         catch(Exception e)
         {
